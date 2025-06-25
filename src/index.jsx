3e0f763ba49cf2daea47853853c2dd83373133d4
halitdip <59334155+halitdip@users.noreply.github.com>
2025-06-25 11:00:04 +0300

    Use MUI for UI components

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Button from './components/Button';
+import Button from '@mui/material/Button';
 
 import Home from './pages/Home';
 import About from './pages/About';
