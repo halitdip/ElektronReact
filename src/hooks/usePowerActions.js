6976f68e9dff346d16849c10e9761ad3e47c4d90
halitdipa101 <halit.dip@a101.com.tr>
2025-07-08 11:34:47 +0300

    update poweractions

--- src/hooks/usePowerActions.js
+++ src/hooks/usePowerActions.js
@@ -1,8 +1,36 @@
 const { exec } = window.require('child_process');
 const os = window.require('os');
+ 
+function getPowerCommand(type) {
+  const platform = os.platform();
+
+  const commands = {
+    shutdown: {
+      win32: 'shutdown /s /t 0',
+      darwin: "osascript -e 'tell app \"System Events\" to shut down'",
+      linux: 'shutdown -h now'
+    },
+    restart: {
+      win32: 'shutdown /r /t 0',
+      darwin: "osascript -e 'tell app \"System Events\" to restart'",
+      linux: 'shutdown -r now'
+    },
+    switchUser: {
+      win32: 'tsdiscon',
+      darwin: '/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend',
+      linux: 'dm-tool switch-to-greeter'
+    }
+  };
+ 
+  return commands[type]?.[platform];
+}
 
 export default function usePowerActions() {
   const runCommand = cmd => {
+    if (!cmd) {
+      console.error('Bu işlem bu platformda desteklenmiyor.');
+      return;
+    }
     exec(cmd, err => {
       if (err) {
         console.error('Power command failed:', err);
@@ -10,44 +38,9 @@ export default function usePowerActions() {
     });
   };
 
-  const handleShutdown = () => {
-    const platform = os.platform();
-    let cmd;
-    if (platform === 'win32') {
-      cmd = 'shutdown /s /t 0';
-    } else if (platform === 'darwin') {
-      cmd = "osascript -e 'tell app \"System Events\" to shut down'";
-    } else {
-      cmd = 'shutdown -h now';
-    }
-    runCommand(cmd);
-  };
-
-  const handleRestart = () => {
-    const platform = os.platform();
-    let cmd;
-    if (platform === 'win32') {
-      cmd = 'shutdown /r /t 0';
-    } else if (platform === 'darwin') {
-      cmd = "osascript -e 'tell app \"System Events\" to restart'";
-    } else {
-      cmd = 'shutdown -r now';
-    }
-    runCommand(cmd);
-  };
-
-  const handleSwitchUser = () => {
-    const platform = os.platform();
-    let cmd;
-    if (platform === 'win32') {
-      cmd = 'tsdiscon';
-    } else if (platform === 'darwin') {
-      cmd = '/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend';
-    } else {
-      cmd = 'dm-tool switch-to-greeter';
-    }
-    runCommand(cmd);
-  };
+  const handleShutdown = () => runCommand(getPowerCommand('shutdown'));
+  const handleRestart = () => runCommand(getPowerCommand('restart'));
+  const handleSwitchUser = () => runCommand(getPowerCommand('switchUser'));
 
   return { handleShutdown, handleRestart, handleSwitchUser };
 }
